1690e3f3c76c94b75881f3f542b60ec2dc63a452
Boris K <bdk898@gmail.com>
2019-04-10 18:12:14 -0400

    Removed local assembly references

--- Services/Personnel/Personnel.Infrastructure/Services/ResumePersisterService.cs
+++ Services/Personnel/Personnel.Infrastructure/Services/ResumePersisterService.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 
 namespace Personnel.Infrastructure.Services
